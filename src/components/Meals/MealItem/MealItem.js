ef16a8ab9378983be305c057691d704f28a97be3
yutanoda <atuyuta902@gmail.com>
2021-07-23 12:08:11 +0900

    142

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,9 @@
+import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
 
 const MealItem = props => {
     const price = `$${props.price.toFixed(2)}`;
-    
+
     return (
         <li className={classes.meal}>
             <div>
@@ -10,7 +11,9 @@ const MealItem = props => {
                 <div className={classes.description}>{props.description}</div>
                 <div className={classes.price}>{price}</div>
             </div>
-            <div></div>
+            <div>
+                <MealItemForm id={props.id} />
+            </div>
         </li>
     );
 };
